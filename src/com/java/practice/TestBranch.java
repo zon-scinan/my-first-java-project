9b282dfabfb079806113064ebd9d0f9f836d2a14
Su Nandar (zon_scinan) <zonscinan91481@gmail.com>
2025-12-04 13:56:15 +0630

    Initial project setup with practice codes

---
+++ src/com/java/practice/TestBranch.java
@@ -0,0 +1,10 @@
+package src.com.java.practice;
+
+public class TestBranch {
+
+        public static void main(String[]args){
+            System.out.println("Hello, Java, from the new branch.☕");
+
+        }
+    }
+
